804f4c4955df5b9cf9777c9e8dc418f8d94f5371
Akif Kadıoğlu <akif.kadioglu.28@gmail.com>
2023-05-30 18:19:08 +0300

    build ayarlarını güncelledim

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'esm',
+      },
+    },
+  },
   plugins: [
     vue(),
     VitePWA(),
