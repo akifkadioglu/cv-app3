40aa583ede8c81133c2cf09254b1dfebdab4b38c
Akif Kadıoğlu <akif.kadioglu.28@gmail.com>
2023-06-02 21:12:48 +0300

    some fixes for UI

--- src/components/CustomCard.vue
+++ src/components/CustomCard.vue
@@ -9,7 +9,7 @@
     </h3>
     
     <div
-      v-html="content"
+      v-html="content.substring(0, 57)+'...'"
       class="line-clamp-2 font-mono text-slate-500 dark:text-slate-400 mt-2 text-sm"
     ></div>
     <div class="flex justify-end mt-5">
