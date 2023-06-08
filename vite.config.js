0e2da26a1480421efac4647f08594e5a2a409d07
Akif Kadıoğlu <akif.kadioglu.28@gmail.com>
2023-06-08 07:14:37 +0300

    sitemap fixed

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,21 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 import sitemap from 'vite-plugin-sitemap';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'esm',
-      },
-    },
-  },
   plugins: [
     vue(),
     VitePWA(),
     sitemap({
       base: 'https://www.akifkadioglu.dev/',
       exclude: [],
-      routes: async () => {
-        return [
-          { url: '/', changefreq: 'weekly', priority: 1.0 },
-          { url: '/projects', changefreq: 'weekly', priority: 0.8 },
-          { url: '/resume', changefreq: 'monthly', priority: 0.9 },
-          { url: '/blog', changefreq: 'daily', priority: 0.5 },
-        ];
-      },
+      routes: [
+        { url: '/', changefreq: 'weekly', priority: 1.0 },
+        { url: '/projects', changefreq: 'weekly', priority: 0.8 },
+        { url: '/resume', changefreq: 'monthly', priority: 0.9 },
+        { url: '/blog', changefreq: 'daily', priority: 0.5 },
+      ],
     }),
   ],
-})
+});
