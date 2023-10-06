a9c46926e3a0e0e7ad96b61befa5aceec9a200f8
Akif Kadıoğlu <akif.kadioglu.28@gmail.com>
2023-10-06 21:13:18 +0300

    added a media and project

--- src/pages/Projects/View.vue
+++ src/pages/Projects/View.vue
@@ -19,7 +19,14 @@ export default {
     return {
       projects: [
         {
-          content: "SecretChat allows you to talk to the people you want without saving your messages.",
+          content:
+            "The traveling salesman problem (TSP) is an algorithmic problem tasked with finding the shortest route between a set of points and locations that must be visited. In the problem statement, the points are the cities a salesperson might visit. The salesman‘s goal is to keep both the travel costs and the distance traveled as low as possible.",
+          title: "Travelling Salesman Problem",
+          link: "https://travelling-salesman-problem.netlify.app/",
+        },
+        {
+          content:
+            "SecretChat allows you to talk to the people you want without saving your messages.",
           title: "SecretChat",
           link: "https://talk-secretly.netlify.app/",
         },
